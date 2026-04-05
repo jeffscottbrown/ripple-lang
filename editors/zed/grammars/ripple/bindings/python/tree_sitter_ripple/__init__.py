c82fbd46c14148303a726c71736081a7c538287a
Jeff Brown <brownj@objectcomputing.com>
2026-04-05 14:03:11 -0500

    Remove Zed extension (moved to ripple-zed repo)

--- editors/zed/grammars/ripple/bindings/python/tree_sitter_ripple/__init__.py
+++
@@ -1,5 +0,0 @@
-"Ripple grammar for tree-sitter"
-
-from ._binding import language
-
-__all__ = ["language"]
--- editors/zed/languages/ripple/outline.scm
+++
@@ -1,8 +0,0 @@
-; Ripple document outline for Zed
-
-(section
-  (section_header
-    (identifier) @name)) @item
-
-(assignment_statement
-  target: (identifier) @name) @item
